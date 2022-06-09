1c3f5ab1f3a205ba40d9b8394f224989011a4f02
Bray Polkinghorne <braypolk@gmail.com>
2022-06-08 23:43:11 -0400

    before I break things, testing active nav

--- src/App.js
+++ src/App.js
@@ -1,35 +1,64 @@
+import React, { useRef, useEffect } from "react";
+import ScrollHighlightNabbar from "./ScrollHighlightNavbar";
+
 import Experience from './Experience';
+import DownloadButton from './assets/download';
+
 import './App.css';
 
 // https://mycareer.qodeinteractive.com/maria/
 
 function App() {
+  const about = useRef();
+  const experience = useRef();
+  const contact = useRef();
+
+  const navHeader = [
+    {
+      headerTitle: "About",
+      headerRef: about,
+      headerID: "about"
+    },
+    {
+      headerTitle: "Experience",
+      headerRef: experience,
+      headerID: "experience"
+    }
+    // {
+    //   headerTitle: "Section 3",
+    //   headerRef: contact,
+    //   headerID: "contact"
+    // }
+  ];
+
   return (
     <div className="app">
       <header className="sidebar">
         <div className="allCenter picker">
-          <nav>
+          <ScrollHighlightNabbar  navHeader={navHeader} />
+          {/* <Scrollspy sectionRefs={sectionRefs}>
             <ul className="navList">
               <li><a href="">About</a></li>
               <li><a href="">Experience</a></li>
               <li><a href="">Contact</a></li>
             </ul>
-          </nav>
+          </Scrollspy> */}
         </div>
-        <div className="downloadSection">
-          <a href="">
-            <svg></svg>
-            <span>Download Resume</span>
+        <div className="downloadSection allCenter">
+          <a className="" href="">
+            <p>Download<br/>Resume</p>
+            <div className="fullWidth allCenter breath2"><DownloadButton/></div>
           </a>
         </div>
       </header>
       <div className="wrapper">
         <div className="heroPage">
+          <div className="image absoluteCenter"></div>
           <div className="bigFirstName">
             BRAY POLKINGHORNE
           </div>
           <div className="someInfo">
-            <p>Full-stack Developer, <br/>Always Learning</p>
+            <p>Full-stack Developer<br/>Always Learning</p>
             <h3 className="action">Learn more below</h3>
           </div>
 
@@ -37,8 +66,8 @@ function App() {
               Polkinghorne
             </div> */}
         </div>
-        <div className="around topPad">
-          <div className="about topPad">
+        <div className="around topPad" ref={about} id="about">
+          <div className="about topPad bottomPad">
             <h1 className="allCenter spaceRight">About Me</h1>
             {/* 
               crypto
@@ -50,7 +79,7 @@ function App() {
             <p className="spaceLeft">Technology, art, and the outdoors. What a weird combo, but those are things I love. Technology and art have this weird relationship of being opposite yet connected. During college, this opposition morphed into a compatibility that I'm so fortunate I found. I studied computer science and minored in photography at Montana State University. What a beautiful state! This satisfied the three things I mentioned earlier and gave me insight to the differences and similarities of engineers and artists. </p>
           </div>
 
-          <div className="experience horizCenter topPad">
+          <div className="experience horizCenter topPad" ref={experience} id="experience">
             <h1 className="bottomPad">Professional Experience</h1>
             <Experience />
           </div>
--- src/Experience.js
+++ src/Experience.js
@@ -1,15 +1,17 @@
 const items = [
     {
         company: "Maven Wave",
+        letters: ["MW"],
         date: "June 2021 - Present",
-        title: "DevOps Analyst",
+        title: "Analyst, DevOps",
         points: [
             "Worked with Capital One to re-platform infrastructure of acquired company. Ensuring security, stability, minimal down time, and compliance with organization",
-            "Major role in deployment and improvement of multi-tenant, HITRUST environments for Mayo Clinic. Requiring deep understanding of large scale terraform deployment, organizational policies, and the ability to pivot based on customer needs."
+            "Major role in deployment and improvement of multi-tenant environments for Mayo Clinic. Requiring deep understanding of large scale terraform deployment, organizational policies, and the ability to pivot based on customer needs."
         ]
     },
     {
         company: "Yellowstone Ecological Research Center",
+        letters: ["YE", "RC"],
         date: "September - May 2020",
         title: "Computer Science Intern, Senior Capstone Team",
         points: [
@@ -19,6 +21,7 @@ const items = [
     },
     {
         company: "Zoot Enterprises",
+        letters: ["ZE"],
         date: "June - August 2020",
         title: "Computer Science Intern, Microservices Team",
         points: [
@@ -29,16 +32,18 @@ const items = [
     },
     {
         company: "Nextworld",
+        letters: ["NW"],
         date: "May - July 2019",
         title: "Computer Science Intern, Integration & Implementation Team",
         points: [
-            "Developed the full stack of a website to train customers on various functionality of Nextworld's enterprise platform; presented to company executives, including the CEO, using PowerPoint and a live demonstration.",
+            "Developed the full-stack of a website to train customers on various functionality of Nextworld's enterprise platform; presented to company executives, including the CEO, using PowerPoint and a live demonstration.",
             "Built strong relationships with other teams and departments, which was critical to implementing projects on-time.",
             "Integrated cross-reference functionality for general ledger accounts in partnership with finance."
         ]
     },
     {
         company: "Bray Polkinghorne Enterprises",
+        letters: ["BP"],
         date: "2017 - 2021",
         title: "Sole Proprietorship",
         points: [
@@ -47,18 +52,34 @@ const items = [
         ]
     }
 ]
+// font-size: 195px;
 
 function Experience() {
     return (
         <>
             {items.map(function (object, i) {
+                const s = 195 / object.letters.length;
+                let size = s.toString()+"px";
+                let height = object.letters.length == 1 ? true : false;
                 return (
                     <div className="block">
                         <div className="info allCenter">
-                            <div className="headingInfo spaceRight">
-                                <h2 className="company bigBreath">{object.company}</h2>
-                                <p className="date breath">{object.date}</p>
-                                <p className="title breath">{object.title}</p>
+                            <div className="headingInfo spaceRight above">
+                                <div className="backLetters">
+                                    {object.letters.map(function (letter, z) {
+                                        if (height) {
+                                            return <p style={{fontSize:size, lineHeight: "1em" }}>{letter}</p>
+                                        }
+                                        else {
+                                            return <p style={{fontSize:size, textAlign: "center"}}>{letter}</p>
+                                        }
+                                    })}
+                                </div>
+                                <div className="above">
+                                    <h2 className="company bigBreath">{object.company}</h2>
+                                    <p className="date breath">{object.date}</p>
+                                    <p className="title breath">{object.title}</p>
+                                </div>
                             </div>
                             <div className="box breath">
                                 <ul className="jobPoints">
@@ -66,7 +87,7 @@ function Experience() {
                                         return (
                                             <li key={z}>{o}</li>
                                         )
-                                    })} 
+                                    })}
                                 </ul>
                             </div>
                         </div>
