5df9f9be9d9d3df9a79dfdab03d84426d5a74f72
Bray Polkinghorne <braypolk@gmail.com>
2022-06-09 23:53:51 -0400

    eod

--- src/App.js
+++ src/App.js
@@ -76,8 +76,8 @@ function App() {
               infra stuff
               web dev stuff
             */}
-
-            <p className="aboutText spaceLeft">Technology, art, and the outdoors. What a weird combo, but those are things I love. Technology and art have this weird relationship of being opposite yet connected. During college, this opposition morphed into a compatibility that I'm so fortunate I found. I studied computer science and minored in photography at Montana State University. What a beautiful state! This satisfied the three things I mentioned earlier and gave me insight to the differences and similarities of engineers and artists. </p>
+            {/*todo string together with outdoors */}
+            <p className="aboutText spaceLeft">Technology, art, and the outdoors are things I love. Technology and art have a relationship of being opposite yet connected. During college, this opposition morphed into a compatibility that I'm so fortunate I found. I studied computer science and minored in photography at Montana State University. What a beautiful state! This satisfied the three things I mentioned earlier and gave me insight to the differences and similarities of engineers and artists. </p>
           </div>
 
           <div className="experience horizCenter topPad" ref={experience} id="experience">
