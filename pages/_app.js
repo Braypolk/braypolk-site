7fa5d50101d30d26f6a80c95c4b4f331d79473fc
Bray Polkinghorne <braypolk@gmail.com>
2022-11-15 12:22:25 -0800

    add card

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import '../styles/App.css';
 import '../styles/Burger.css';
-import '../styles/card.min.css';
+// import '../styles/card.min.css';
 import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
