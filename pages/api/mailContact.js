5bc0e63b7cc5e8078cc95cad47a82a2e771bc83e
Bray Polkinghorne <braypolk@gmail.com>
2022-11-25 15:39:16 -0700

    updated mailcontact sent info

--- pages/api/mailContact.js
+++ pages/api/mailContact.js
@@ -1,84 +1,103 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { processEnv } from '@next/env';
-
+import { processEnv } from '@next/env'
+import fs from 'fs'
 const mail = require('@sendgrid/mail')
 
-function ValidateEmail(mail) 
-{
- if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,7})+$/.test(mail))
-  {
-    return (true)
+function ValidateEmail (mail) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,7})+$/.test(mail)) {
+    return true
   }
-  return (false)
+  return false
 }
 
-mail.setApiKey(process.env.SENDGRID_API_KEY);
-export default function handler(req, res) {
-  const body = JSON.parse(req.body);
+mail.setApiKey(process.env.SENDGRID_API_KEY)
+export default function handler (req, res) {
+  const body = JSON.parse(req.body)
 
   // check if form is empty
   for (const key in body) {
     console.log(key, body[key])
-    if (body[key] === "" || !body[key]) {
-      return res.status(422).json({message:'Form value is empty'})
+    if (body[key] === '' || !body[key]) {
+      return res.status(422).json({ message: 'Form value is empty' })
     }
-  };
+  }
 
+  let data,
+    dataToMe = {}
   // check if email is valid
   let emailStatus = ValidateEmail(body.email)
   if (!emailStatus) {
-    return res.status(422).json({message:'You have entered an invalid email address!'})
+    return res
+      .status(422)
+      .json({ message: 'You have entered an invalid email address!' })
   }
 
-  const message = `
-    Great meeting you today!\r\n
-    Name: Bray Polkinghorne\r\n
-    Email: braypolk@gmail.com\r\n
-  `;
-
-  const data = {
-    to: body.email,
-    subject: 'Great Meeting You',
-    from: 'brayPolkSite@bray.dev',
-    text: message,
-    html: message.replace(/\r\n/g, '<br/>')
-  };
-
-  const messageToMe = `
-    Name: ${body.name}\r\n
-    Email: ${body.email}\r\n
-    Phone: ${body.phone}
-  `;
-
-  const dataToMe = {
-    to: 'braypolk@gmail.com',
-    subject: 'New Message From BrayPolk Site',
-    from: 'brayPolkSite@bray.dev',
-    text: messageToMe,
-    html: messageToMe.replace(/\r\n/g, '<br/>')
-  };
-console.log(data);
-  
-  (async () => {
-    try {
-      await mail.send(data);
-      try {
-        await mail.send(dataToMe);
-      } catch (error) {
-        console.error(error);
-      if (error.response) {
-        console.error(error.response.body)
-        return res.status(422).json({message:'Something went wrong'})
-      }
-      }
-      return res.status(200).json({message:'Looks good'})
-    } catch (error) {
-      console.error(error);
-      if (error.response) {
-        console.error(error.response.body)
-        return res.status(422).json({message:'Something went wrong'})
-      }
+  fs.readFile(process.cwd() + '/public/assets/contact.vcf', (err, file) => {
+    if (err) {
+      // do something with the error
     }
-  })();
+    if (file) {
+      data = {
+        to: body.email,
+        from: 'brayPolkSite@bray.dev',
+        templateId: 'd-ecb9ec5ba883434f91b1c72bd370fadf',
+        attachments: [
+          {
+            content: file.toString('base64'),
+            filename: 'brayContactCard.vcf',
+            type: 'text/vCard',
+            disposition: 'attachment',
+            content_id: 'brayContactCard'
+          }
+        ]
+      }
+
+
+      const messageToMe = `Name: ${body.name}\r\nEmail: ${body.email}\r\nPhone: ${body.phone}`
+      const personContact = `BEGIN:VCARD\r\nVERSION:2.1\r\nFN:${body.name}\r\nTEL;CELL:${body.phone}\r\nEMAIL:${body.email}\r\nEND:VCARD`
+
+      dataToMe = {
+        to: 'braypolk@gmail.com',
+        subject: 'New Contact From BrayPolk Site',
+        from: 'brayPolkSite@bray.dev',
+        text: messageToMe,
+        html: messageToMe.replace(/\r\n/g, '<br/>'),
+        attachments: [
+          {
+            content: btoa(personContact),
+            filename: `${body.name}Contact.vcf`,
+            type: 'text/vCard',
+            disposition: 'attachment',
+            content_id: 'brayContactCard'
+          }
+        ]
+      }
+
+      mail
+        .send(data)
+        .then(() => console.log('Mail sent successfully'))
+        .catch(error => {
+          console.error(error)
+
+          if (error.response) {
+            console.error(error.response.body)
+          }
+          return res.status(422).json({ message: 'Something went wrong' })
+        })
+      mail
+        .send(dataToMe)
+        .then(() => {
+          return res.status(200).json({ message: 'Looks good' })
+        })
+        .catch(error => {
+          console.error(error)
+
+          if (error.response) {
+            console.error(error.response.body)
+          }
+          return res.status(422).json({ message: 'Something went wrong' })
+        })
+    }
+  })
 }
