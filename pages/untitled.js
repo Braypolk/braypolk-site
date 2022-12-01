9263eecfddb94cd5c0b19f58ae54fffdd4057b81
Bray Polkinghorne <braypolk@gmail.com>
2022-11-30 16:47:09 -0800

    test csr

--- pages/untitled.js
+++ pages/untitled.js
@@ -2,6 +2,7 @@ import * as Y from 'yjs'
 import { WebrtcProvider } from 'y-webrtc'
 import { useEffect } from 'react'
 
+const SSR = typeof window === 'undefined'
 const ydoc = new Y.Doc()
 // const provider = new WebrtcProvider(`tutorial-tic-tac-toe`, ydoc)
 const provider = new WebrtcProvider('your-room-name', ydoc)
@@ -25,7 +26,6 @@ const provider = new WebrtcProvider('your-room-name', ydoc)
 //     }
 //     return null;
 //   }
-  
 
 // function Square (props) {
 //   return (
@@ -67,97 +67,101 @@ const provider = new WebrtcProvider('your-room-name', ydoc)
 // }
 
 function Untitled ({ Component, pageProps }) {
-//   useEffect(() => {
-//     const ymap = ydoc.getMap('state')
-//     ymap.observe(() => {
-//       this.setState({
-//         ...ymap.get('state')
-//       })
-//       console.log('helo', ymap.get('state'))
-//     })
+  //   useEffect(() => {
+  //     const ymap = ydoc.getMap('state')
+  //     ymap.observe(() => {
+  //       this.setState({
+  //         ...ymap.get('state')
+  //       })
+  //       console.log('helo', ymap.get('state'))
+  //     })
 
-//     return () => {
-//       // the same as coponentwillunmount
-//       provider.disconnect()
-//     }
-//   })
-//   handleClick((i) => {
-//     const history = this.state.history.slice(0, this.state.stepNumber + 1)
-//     const current = history[history.length - 1]
-//     const squares = current.squares.slice()
-//     if (calculateWinner(squares) || squares[i]) {
-//       return
-//     }
-//     squares[i] = this.state.xIsNext ? 'X' : 'O'
-//     this.setState(
-//       {
-//         history: history.concat([
-//           {
-//             squares: squares
-//           }
-//         ]),
-//         stepNumber: history.length,
-//         xIsNext: !this.state.xIsNext
-//       },
-//       () => {
-//         const ymap = ydoc.getMap('state')
-//         ymap.set('state', this.state)
-//       }
-//     )
-//   })
+  //     return () => {
+  //       // the same as coponentwillunmount
+  //       provider.disconnect()
+  //     }
+  //   })
+  //   handleClick((i) => {
+  //     const history = this.state.history.slice(0, this.state.stepNumber + 1)
+  //     const current = history[history.length - 1]
+  //     const squares = current.squares.slice()
+  //     if (calculateWinner(squares) || squares[i]) {
+  //       return
+  //     }
+  //     squares[i] = this.state.xIsNext ? 'X' : 'O'
+  //     this.setState(
+  //       {
+  //         history: history.concat([
+  //           {
+  //             squares: squares
+  //           }
+  //         ]),
+  //         stepNumber: history.length,
+  //         xIsNext: !this.state.xIsNext
+  //       },
+  //       () => {
+  //         const ymap = ydoc.getMap('state')
+  //         ymap.set('state', this.state)
+  //       }
+  //     )
+  //   })
 
-//   jumpTo(step => {
-//     this.setState({
-//       stepNumber: step,
-//       xIsNext: step % 2 === 0
-//     })
-//   })
+  //   jumpTo(step => {
+  //     this.setState({
+  //       stepNumber: step,
+  //       xIsNext: step % 2 === 0
+  //     })
+  //   })
 
-//   renderSquare(i => {
-//     return (
-//       <Square
-//         value={this.props.squares[i]}
-//         onClick={() => this.props.onClick(i)}
-//       />
-//     )
-//   })
+  //   renderSquare(i => {
+  //     return (
+  //       <Square
+  //         value={this.props.squares[i]}
+  //         onClick={() => this.props.onClick(i)}
+  //       />
+  //     )
+  //   })
 
-//   const history = this.state.history
-//   const current = history[this.state.stepNumber]
-//   const winner = calculateWinner(current.squares)
+  //   const history = this.state.history
+  //   const current = history[this.state.stepNumber]
+  //   const winner = calculateWinner(current.squares)
 
-//   const moves = history.map((step, move) => {
-//     const desc = move ? 'Move #' + move : 'Game start'
-//     return (
-//       <li key={move}>
-//         <a href='#' onClick={() => this.jumpTo(move)}>
-//           {desc}
-//         </a>
-//       </li>
-//     )
-//   })
+  //   const moves = history.map((step, move) => {
+  //     const desc = move ? 'Move #' + move : 'Game start'
+  //     return (
+  //       <li key={move}>
+  //         <a href='#' onClick={() => this.jumpTo(move)}>
+  //           {desc}
+  //         </a>
+  //       </li>
+  //     )
+  //   })
 
-//   let status
-//   if (winner) {
-//     status = 'Winner: ' + winner
-//   } else {
-//     status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
-//   }
+  //   let status
+  //   if (winner) {
+  //     status = 'Winner: ' + winner
+  //   } else {
+  //     status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+  //   }
 
   return (
-    <div className='game'>
-        test
-      <div className='game-board'>
-        {/* <Board
+    <>
+      {!SSR ? (
+        <div className='game'>
+          test
+          <div className='game-board'>
+            {/* <Board
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
-          /> */}
-      </div>
-      <div className='game-info'>
-        {/* <div>{status}</div> */}
-        {/* <ol>{moves}</ol> */}
-      </div>
-    </div>
+        /> */}
+          </div>
+          <div className='game-info'>
+            {/* <div>{status}</div> */}
+            {/* <ol>{moves}</ol> */}
+          </div>
+        </div>
+      ) : null}
+    </>
   )
 }
 
